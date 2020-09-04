67024e87bdee9ebd6eea9198db4c8f69389fe695
firefox66 <nguyenduy1986@gmail.com>
2020-09-04 06:46:35 +0000

    makefile update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
+
 #include <iostream>
 
 using namespace std;
 
-int main(){
-	cout << "Hello, World...";
-	return 0;
+int main(int argc, char const *argv[])
+{
+   cout << "Hello, Duy...";
+   return 0;
 }
